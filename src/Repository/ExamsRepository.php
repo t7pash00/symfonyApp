80aa22abad51c1981e5185bc08ad245aa456ea2a
Shyamraja <t7bhsh00@students.oamk.fi>
2018-10-13 18:26:25 +0300

    Trying to fix exam repository with queries

--- src/Repository/ExamsRepository.php
+++ src/Repository/ExamsRepository.php
@@ -22,8 +22,57 @@ class ExamsRepository extends ServiceEntityRepository
 //    /**
 //     * @return Exams[] Returns an array of Exams objects
 //     */
-    /*
-    public function findByExampleField($value)
+    
+    public function GetAllExams($userid):array{
+        $conn = $this->getEntityManager()->getConnection();
+
+        $sql = '
+          SELECT
+          id,
+          edescription,
+          ecategory,
+          CASE
+          WHEN ecategory =1  THEN "Programming"
+          WHEN ecategory =2  THEN "Math"
+          WHEN ecategory =3  THEN "Finnish Languge"
+          ELSE "No category is selected"
+          END as qcategoryDsc,
+          qdifficultylevel,
+          CASE
+          WHEN edifficultylevel =0  THEN "Easy"
+          WHEN edifficultylevel =1  THEN "Medium"
+          WHEN edifficultylevel =2  THEN "Hard"
+          ELSE "No edifficultylevel is selected"
+          END as edifficultylevelDsc,
+
+          userid 
+          FROM examss e
+          WHERE e.userid = :userid
+          ORDER BY e.qcategory
+          ';
+      $stmt = $conn->prepare($sql);
+      $stmt->execute(['userid' => $userid]);
+     // returns an array of arrays (i.e. a raw data set)
+    return $stmt->fetchAll();
+    }
+    public function GetExamResultss($eid):array{
+        $conn = $this->getEntityManager()->getConnection();
+
+        $sql = '
+            SELECT 
+            id, examid, ansdescription,  iscorrect,
+            CASE
+            WHEN iscorrect =0  THEN "Wrong"
+            WHEN iscorrect =1  THEN "Correct"
+            END as iscorrectDsc
+            FROM exams a
+            WHERE a.examid= :eid
+            ';
+            $stmt = $conn->prepare($sql);
+            $stmt->execute(['eid' => $eid]);
+    
+        // returns an array of arrays (i.e. a raw data set)
+        return $stmt->fetchAll();
     {
         return $this->createQueryBuilder('e')
             ->andWhere('e.exampleField = :val')
